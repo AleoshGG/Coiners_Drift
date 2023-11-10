4bba9a80271404846c1d8f1e0d3a449636d1dd8f
alejandroimen <149030059+alejandroimen@users.noreply.github.com>
2023-11-10 08:12:28 -0600

    Add files via upload

---
+++ Coiner Drift/power_up/index.js
@@ -0,0 +1,4 @@
+document.getElementById("btnEvens").addEventListener("click", function() {
+    // Código para abrir la página web en la misma pestaña
+    window.location.href = "C:\Users\richa\OneDrive\Escritorio\Coiner Drift\evens"; // Reemplaza con el nombre de tu archivo HTML
+});
